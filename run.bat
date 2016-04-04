8a48226d6b1b75e0496340ceb83986d5db34e0a7
Marius Gedminas <marius@gedmin.as>
2016-04-04 09:53:27 +0300

    Add all the intermediates

--- run.bat
+++ run.bat
@@ -2,4 +2,4 @@ cd %~dp0
 if "%JAVA_HOME%"=="" set JAVA_HOME=c:\Program Files (x86)\Java\jre7
 if "%KEYSTORE%"=="" set KEYSTORE=%JAVA_HOME%\lib\security\cacerts
 echo Importing LetsEncrypt root cert into %KEYSTORE% >> run.log
-for %%i in (isrgrootx1.der) do "%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%KEYSTORE%" -storepass changeit -noprompt -alias %%~ni -file %%i >> run.log
+for %%i in (*.der) do "%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%KEYSTORE%" -storepass changeit -noprompt -alias %%~ni -file %%i >> run.log
