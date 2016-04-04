ad8a4406b9e536d95c898b7d46d025f00a9c9af9
Marius Gedminas <marius@gedmin.as>
2016-04-04 09:42:36 +0300

    Fix syntax errors

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 cd %~dp0
-if "%JAVA_HOME%=="" set JAVA_HOME=c:\Program Files (x86)\Java\jre7
-if "%KEYSTORE%=="" set KEYSTORE=%JAVA_HOME%\lib\security\cacerts
+if "%JAVA_HOME%"=="" set JAVA_HOME=c:\Program Files (x86)\Java\jre7
+if "%KEYSTORE%"=="" set KEYSTORE=%JAVA_HOME%\lib\security\cacerts
 echo "Importing LetsEncrypt root cert into %KEYSTORE%" >> run.log
 for %%i in (isrgrootx1.der) do "%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias %%~ni -file %%i >> run.log
