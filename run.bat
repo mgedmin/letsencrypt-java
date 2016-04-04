fe95b0f67916aea957cd9fdba9d5aa73f6e88b39
Marius Gedminas <marius@gedmin.as>
2016-04-04 09:37:21 +0300

    Rewrite for LetsEncrypt

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 cd %~dp0
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca -file ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class1 -file sub.class1.server.ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class2 -file sub.class2.server.ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class3 -file sub.class3.server.ca.crt >> run.log
-"%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias startcom.ca.sub.class4 -file sub.class4.server.ca.crt >> run.log
+if "%JAVA_HOME%=="" set JAVA_HOME=c:\Program Files (x86)\Java\jre7
+if "%KEYSTORE%=="" set KEYSTORE=%JAVA_HOME%\lib\security\cacerts
+echo "Importing LetsEncrypt root cert into %KEYSTORE%" >> run.log
+for %%i in (isrgrootx1.der) do "%JAVA_HOME%\bin\keytool" -import -trustcacerts -keystore "%JAVA_HOME%\jre\lib\security\cacerts" -storepass changeit -noprompt -alias %%~ni -file %%i >> run.log
